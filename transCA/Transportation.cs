9725fff99d49c40a9d249a88b6b51b0d01391e75
Alex <alexander_sck@live.ca>
2022-03-31 00:01:25 -0400

    feat: create abstract transportation class

---
+++ transCA/Transportation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace transCA
+{
+    public abstract class Transportation
+    {
+        protected int _ticketPrice;
+        protected int _speed;
+        protected int _passengers;
+        protected int _distance;
+        protected double _total;
+        protected DateTime _arrival;
+        public abstract double GetTotal();
+        public abstract DateTime GetArrival();
+        
+    }
+}
