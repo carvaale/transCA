0b8baf88acac07ef8d014c736d4fa462d225e7c2
Alex <alexander_sck@live.ca>
2022-03-31 00:08:25 -0400

    feat: create plane class (inherits from transportation) and destination class

---
+++ transCA/Destination.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace transCA
+{
+    public class Destination
+    {
+        private int _distance; //Distance in KM
+        public int Distance{ get { return _distance; } }
+
+        public Destination(string province)
+        {
+            _distance = GetProvinceDistance(province);
+        }
+
+        private int GetProvinceDistance(string province)
+        {
+            switch(province)
+            {
+                case "yukon":
+                    return 4000;
+                case "british columbia":
+                    return 3400 ;
+                case "northwest territories":
+                    return 3100;
+                case "alberta":
+                    return 2700;
+                case "saskatchewan":
+                    return 2200;
+                case "nunavut":
+                    return 2300;
+                case "manitoba":
+                    return 1500;
+                case "quebec":
+                    return 760;
+                case "newfoundland and labrador":
+                    return 2200;
+                case "prince edwards island":
+                    return 1300;
+                case "new brunswick":
+                    return 1100;
+                case "nova scotia":
+                    return 2000;
+                default:
+                    return 0;
+                    
+            }     
+        }
+    }
+}
---
+++ transCA/Plane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace transCA
+{
+    public class Plane : Transportation
+    {
+
+        public double Total { get { return _total; } }
+        public DateTime Arrival { get { return _arrival; } }
+
+        public Plane(Destination destination, int passengers)
+        {
+            _ticketPrice = 250;
+            _speed = 800; //Speed of plane in KM/H
+            _passengers = passengers;
+            _distance = destination.Distance;
+            _total = GetTotal();
+            _arrival = GetArrival();
+        }
+
+        public override double GetTotal()
+        {
+            return _ticketPrice * _passengers;
+        }
+
+        public override DateTime GetArrival()
+        {
+            return DateTime.Now.AddHours((_distance / _speed) + 5); //Add 5hrs to compensate for time boarding and disembarking flight.
+        }
+    }
+}
