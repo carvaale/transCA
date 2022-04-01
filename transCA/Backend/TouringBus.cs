38a62f4f175ccb17ef0f57678504215fbf5dd9a3
Klajdi <klajdihoxhalli2@gmail.com>
2022-03-31 21:31:04 -0500

    feat:TouringBus child class complete

--- transCA/Backend/TouringBus.cs
+++ transCA/Backend/TouringBus.cs
@@ -6,7 +6,21 @@ namespace transCA.Backend
 {
     internal class TouringBus: Transportation
     {
+        public TouringBus(Destination destinationTouringBus, int passengersTouringBus)
+        {
+            _ticketPrice = destinationTouringBus.DestinationInfo[3];
+            _speed = 100;
+            _passengers = passengersTouringBus;
+            _distance = destinationTouringBus.DestinationInfo[0];
+            _total = GetTotal();
+            _arrival = GetArrival();
 
+        }
+        public override double GetTotal()
+        {
+            return _ticketPrice * _passengers;
+        }
+      
 
 
     }
