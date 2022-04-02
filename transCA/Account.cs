512528bf38f1b7dc5446dad9eb6d0b6340f4f623
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-04-02 13:38:58 -0400

    Add the _name and _balance variable in Account Class

--- transCA/Account.cs
+++ transCA/Account.cs
@@ -4,6 +4,19 @@ namespace transCA
     public class Account
         //Name: Muhammad Choudhary
     {
+
+        //Storing name of user
+        private string _name;
+
+        //Storing user balance
+        private double _balance;
+
+
+
+
+
+
+
         public Account()
         {
         }
