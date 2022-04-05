bc00833b45f1f502a7ed13c656920c793f0f30a3
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-04-05 13:33:43 -0400

    feat: Create train class

--- transCA/Backend/Train.cs
+++ transCA/Backend/Train.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 namespace transCA.Backend
 {
-    public class Train
+    public class Train : Transportation
         //Muhammad Choudhary
     {
-        public Train()
+
+
+
+        public Train(Destination destination, int passengers)
         {
+            _ticketPrice = destination.DestinationInfo[3];
+            _speed = 250; //Speed of plane in KM/H
+            _passengers = passengers;
+            _distance = destination.DestinationInfo[0];
         }
+
+        public override double GetTotal()
+        {
+            return _ticketPrice * _passengers;
+        }
+
+        public override DateTime GetArrival()
+        {
+            return DateTime.Now.AddHours((_distance / _speed) + 2); //Add 2hrs to compensate for time boarding and disembarking train.
+        }
+
+
     }
 }
