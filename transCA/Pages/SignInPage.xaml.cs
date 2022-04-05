739411a0edfce90cab0421e9daeb59ca8e3a81ce
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-04-05 14:14:59 -0400

    feat: Create SignInPage

---
+++ transCA/Pages/SignInPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace transCA.Pages
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
